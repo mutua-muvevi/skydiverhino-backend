2726460ff32cae60c8876304078037cd0480900d
mutua-muvevi <samstore637@gmail.com>
2023-11-06 17:38:20 +0300

    added delete faq controller

--- routes/faq.js
+++ routes/faq.js
@@ -9,11 +9,34 @@ const checkUserExistence = require("../middlewares/checkuser");
 const { createFAQ } = require("../controllers/faq/new");
 const { editFAQ } = require("../controllers/faq/edit");
 const { fetchAllFAQs, fetchFAQByID } = require("../controllers/faq/fetch");
+const { deleteFAQ, deleteFAQs } = require("../controllers/faq/delete");
 
 //routes
 router.post("/:userID/post", authMiddleware, checkUserExistence, createFAQ);
 router.put("/:userID/edit/:FAQID", authMiddleware, checkUserExistence, editFAQ);
-router.get("/:userID/fetch/all", authMiddleware, checkUserExistence, fetchAllFAQs);
-router.get("/:userID/fetch/single/:FAQID", authMiddleware, checkUserExistence, fetchFAQByID);
+router.get(
+	"/:userID/fetch/all",
+	authMiddleware,
+	checkUserExistence,
+	fetchAllFAQs
+);
+router.get(
+	"/:userID/fetch/single/:FAQID",
+	authMiddleware,
+	checkUserExistence,
+	fetchFAQByID
+);
+router.delete(
+	"/:userID/delete/single/:faqID",
+	authMiddleware,
+	checkUserExistence,
+	deleteFAQ
+);
+router.delete(
+	"/:userID/delete/many",
+	authMiddleware,
+	checkUserExistence,
+	deleteFAQs
+);
 
 module.exports = router;
