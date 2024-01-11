8331d55f599d5b38e7e2d9d08c44b633b782b963
mutua-muvevi <samstore637@gmail.com>
2024-01-11 13:34:03 +0300

    made some modifications for premature hosting

--- index.js
+++ index.js
@@ -52,7 +52,23 @@ app.use("/api/homepage", require("./routes/homepage"));
 //error middleware
 app.use(errorHandler);
 
+//define the port
+const PORT = process.env.PORT || 5000;
+
+//listen to the port
+app.listen(PORT, () => logger.info(`Server started on port ${PORT}`));
+
+// process termination after unhandles promise rejection
+process.on("unhandledRejection", (error, promise) => {
+	if(error){
+		logger.error("Unhandled Promise Rejection Error :", error)
+		process.exit(1)
+	} else {
+		logger.info("Unhandled Promise :", promise)
+	}
+})
+
 // port connection
 module.exports = app;
 
-require("./config/port")
+// require("./config/port")
